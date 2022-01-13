368ff5f62fc52cce56ebf2312ca845bf4d740972
h-anjru <andrew.lassiter@yahoo.com>
2022-01-13 07:37:03 -0600

    readme written, code cleaned up

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # uas-oblique-photo
 A simulation of oblique photogrammetry from the UAS platform for accuracy assessment and mission planning.
+
+## Overview
+I wrote these scripts as part of my dissertation research into oblique photogrammetry from drones over forests. Collecting oblique imagery in airborne photogrammetry has been practiced for a long time for ubran areas because it produces better 3D reconstructions of vertical features such as walls. But I had noticed a dearth of studies of oblique photogrammetry from UAS over forests, and I thought it warranted further study.
+
+### Dynamic, close-range photogrammetry
+Conventional aerial photogrammetry practices assume a much higher flying height above ground than those achieved using drones, yet most missions for UAS photogrammetry were being planned using those practices. My thought was that, instead of thinking of it as low-altitude aerial photogrammetry, we should think of drone photogrammetry as *dynamic, close-range photogrammetry.*
+
+![Capturing nadir photos from low altitudes seemed to result in "false positives," or reconstruction of features that simply weren't there. Shown here is a 3D reconstrution of a cluster of trees (left: true color; right: color by height (blue:low > red:high).](figures/false-positives.png)
+
+*Capturing nadir photos from low altitudes seemed to result in "false positives," or reconstruction of features that simply weren't there. Shown here is a 3D reconstrution of a cluster of trees (left: true color; right: color by height (blue:low > red:high).*
+
+I considered the much-shorter average range from camera to target when using a drone versus a manned aircraft. The relief displacement is much mroe exxagerated. In other words, there is an increased target height to flying height ratio (for example, the height of a building in relation to the height of the camera above it is drastically different from a drone than a conventional manned aircraft).
+
+![Uncertainty in reconstruction (Hartley & Zisserman)](figures/uncertainty-in-reconstruction_hartley-zisserman.png)
+
+*Uncertainty in reconstruction (Hartley & Zisserman)*
+
+The two main principles of close-range photogrammetry are
+
+1. varying ranges of targets and 
+2. strong geometry between camera stations.
+
+Both of these can be acheived by capturing oblique images. To go further, both can be achieved using a single camera flown at a constant height above ground.
+
+### Workflow
+Applying these principles to drone photogrammetry should increase the accuracy of the resulting 3D reconstruction of the photos. My thought on the fastest way to test this contention was to run a few simulations of low-altitude, oblique photo capture in MATLAB.
+
+1. Create a matrix of points in object space )roughly the height of the trees typically seen in my study areas)
+2. Set an array of camera stations above these object space points at a given height and tilt
+3. Back-project the object space points onto the camera stations--essentially creating virutal 2D images of the point array
+4. Apply a bundle adjustment on these virtual images and the object space points and analyze the statistics on the adjustment
+
+![Animation of the simulated environment created in MATLAB for testing oblique photo collection from low altitudes.](figures/oblique-flightlines.gif)
+
+*Animation of the simulated environment created in MATLAB for testing oblique photo collection from low altitudes.*
+
+### Supplemental information
+
+You can read more about the simulations, the results, and thir application to real-world photo collects [here](/supplemental).
+
+## missionSim.m
+`missionSim()` creates a small simulated photo collection mission for testing bundle adjustment results with BUN2013.exe (not included in this repository).
+
+### Call
+`missionSim(endlap, sidelap, flying_height, tilt)`
+
+### Input
+`endlap`: endlap from photo to photo (0.0-1.0)
+
+`sidelap`: sidelap from flight line to flight line (0.0-1.0)
+
+`flying_height`: height above ground (meters)
+
+`tilt`: angle of tilt of camera from nadir (deg)
+
+### Output
+The output is an ASCII file (.dat) which is formatted for input into a bundle adjustment software, BUN2013.exe, written by Bon Dewitt.
+
+Reference: Wolf, Dewitt, Wilkinson. "Elements of Photogrammetry 4th ed."
+
+## backProject.m
+`backProject` back projects some array of object space coordinates into two images. This function assumes an ideal pinhole camera. Noise is added to the back-projected image coordinates.
+
+### Call
+`[x1, x2, noise] = backproject(X, cam1, cam2, sigma)`
+
+### Input
+`X`: `[n, n x 3]` matrix of `n` names, object space points of the format `[name, X, Y, Z]`
+
+`cam`: row vector of external orientation parameters (EOPs) for camera `[XL, YL, ZL, omega, phi, kappa, f(pix)]`
+
+`sigma`: stdev of image measurements (typically 0.5 pixel) used to crete Gaussian noise
+
+### Output
+`x`: `2 x n` matrix of image coordinates for cameras 1 & 2 `[x1, y1, x2, y2]`
+
+Reference: Wolf, Dewitt, Wilkinson. "Elements of Photogrammetry 4th ed." Appendix D-4.
--- missionSim.m
+++ missionSim.m
@@ -1,193 +1,294 @@
-function missionSim(endlap,sidelap,flying_height,tilt)
-%MISSIONSIM
-%   
-%  
+function missionSim(endlap, sidelap, flying_height, tilt)
+%MISSIONSIM Creates a small simulated photo collection mission for testing 
+%  bundle adjustment results with BUN2013.exe.
+%
+%  Call: missionSim(endlap, sidelap, flying_height, tilt) 
+%
+%         endlap: endlap from photo to photo (0.0-1.0)
+%        sidelap: sidelap from flight line to flight line (0.0-1.0)
+%  flying_height: height above ground (meters)
+%           tilt: angle of tilt of camera from nadir (deg)
 
-% camera parameters (Canon EOS-Rebel SL1)
-f = 24; format_y = 22.3; format_x = 14.9; pix = 0.0043; % [mm]
-fov = 2*atan2d(format_x,2*f);
-f_pix = f/pix;
+%         output: ASCII file (.dat) which is formatted for input into a bundle
+%                 adjustment software, BUN2013.exe, written by Bon Dewitt.
 
-fprintf('\nf = %i mm\n\n',f);
+%  Reference: Wolf, Dewitt, Wilkinson. "Elements of Photogrammetry 4th ed."
+%
+%  Original equations for GSD, B, and W by Lassiter (2017).
 
-% calculate and plot GSD
-m = round(format_x/pix,0);
-p = 1:m;
-mu_(p) = atan((p-1-m/2)/f_pix);
-mu(p) = atan((p-m/2)/f_pix);
-t = deg2rad(tilt);
-gsd(p) = flying_height*(tan(t+mu(p)) - tan(t+mu_(p)));
-% plot(p,gsd,'LineWidth',3);
-fprintf('GSD in direction of flight:\n')
-fprintf('min = %3.1f cm\n',100*min(gsd))
-fprintf('med = %3.1f cm\n',100*median(gsd))
-fprintf('max = %3.1f cm\n',100*max(gsd))
+    % camera parameters (Canon EOS-Rebel SL1)
+    f = 24;
+    format_y = 22.3;
+    format_x = 14.9;
+    pix = 0.0043;  % [mm]
+    fov = 2*atan2d(format_x, 2 * f);
+    f_pix = f / pix;
 
-% calculate airbase B and flight line spacing W
-B = flying_height*(tand(tilt + fov/2) - tand(tilt - fov/2))*(1 - endlap);
-B = abs(B);
-W = flying_height*(format_y/(f*cosd(tilt)))*(1 - sidelap);
-fprintf('\nAirbase = %7.2f m\nAirwidth = %6.2f m\n',B,W)
+    fprintf('\nf = %i mm\n\n', f);
 
-% set up EOPs; first, where optical axis intersects ground
-x = 100; y = 100;
-X1 = linspace(x,x + 3*B,4); X2 = X1 + B/2;
-Y1 = [y,y + 2*W];Y2 = y + W;
+    % calculate and plot ground sample distance (GSD)
+    m = round(format_x / pix, 0);
+    p = 1:m;
+    mu_(p) = atan((p - 1 - m/2) / f_pix);
+    mu(p) = atan((p - m / 2) / f_pix);
+    t = deg2rad(tilt);
+    gsd(p) = flying_height*(tan(t+mu(p)) - tan(t+mu_(p)));
 
-% offset for tilt 
-offset = flying_height*tand(tilt);
-X1o = X1 - offset; X2o = X2 + offset;
+    % plot(p, gsd, 'LineWidth', 3);
+    fprintf('GSD in direction of flight:\n')
+    fprintf('min = %3.1f cm\n', 100 * min(gsd))
+    fprintf('med = %3.1f cm\n', 100 * median(gsd))
+    fprintf('max = %3.1f cm\n', 100 * max(gsd))
 
-[XL,YL,ZL] = meshgrid(X1o,Y1,flying_height); eop = [XL(:) YL(:) ZL(:)];
-[XL,YL,ZL] = meshgrid(X2o,Y2,flying_height); eop = [eop;XL(:) YL(:) ZL(:)];
+    % calculate airbase B and flight line spacing W
+    B = flying_height * ...
+        (tand(tilt + fov / 2) - tand(tilt - fov / 2)) * ...
+        (1 - endlap);
+    B = abs(B);
+    W = flying_height * ...
+        (format_y / (f * cosd(tilt))) * ...
+        (1 - sidelap);
+    fprintf('\nAirbase = %7.2f m\nAirwidth = %6.2f m\n', B, W)
 
-% get EOPs in serpentine order
-eop = sortrows(eop,2);
-eop(5:8,:) = sortrows(eop(5:8,:),1,'descend');
+    % set up camera stations
+    % first, where optical axis intersects ground
+    x = 100;
+    y = 100;
+    X1 = linspace(x, x + 3 * B, 4);
+    X2 = X1 + B / 2;
+    Y1 = [y, y + 2 * W];
+    Y2 = y + W;
 
-% +/-tilt is a -/+y rotation in this case
-eop = [eop zeros(size(eop)) f_pix*ones(size(eop,1),1)];
-eop(:,5) = -tilt; eop(5:8,5) = tilt;
+    % offset for tilt 
+    offset = flying_height * tand(tilt);
+    X1o = X1 - offset;
+    X2o = X2 + offset;
 
-% point cloud for the testing
-extend = -flying_height/(0.2*f);
-xv = linspace(X2(1) - extend, X1(4) + extend, 3);
-yv = linspace(Y1(1) - extend, Y1(2) + extend, 3);
-zv = linspace(0,20,3);
-[X,Y,Z] = meshgrid(xv,yv,zv); pts = [X(:) Y(:) Z(:)];
-names = 101:101+size(pts,1)-1;
-pts = [names' pts];
+    [XL, YL, ZL] = meshgrid(X1o, Y1, flying_height);
+    cam = [XL(:), YL(:), ZL(:)];
 
-% cell array to store image matrices
-images = cell(size(eop,1),1);
+    [XL_, YL_, ZL_] = meshgrid(X2o, Y2, flying_height);
+    cam = [cam; XL_(:), YL_(:), ZL_(:)];
 
-% call eop iteratively, send to backproject, store results in cell array
-for ii = 1:size(eop,1)
-    images{ii} = backproject(pts,eop(ii,:),0);
-end
+    % get camera stations in serpentine order
+    cam = sortrows(cam, 2);
+    cam(5:8, :) = sortrows(cam(5:8, :), 1, 'descend');
 
-% write results to bun2013.exe dat file
-outfile = ['bun_e' num2str(endlap*100) ...
-    '_s' num2str(sidelap*100) ...
-    '_h' num2str(flying_height) ...
-    '_f' num2str(f) ...
-    '_t' num2str(tilt) '.dat'];
-fid = fopen(outfile,'wt');
+    % +/-tilt is a -/+y rotation in this case
+    cam = [cam, zeros(size(cam)), f_pix * ones(size(cam, 1), 1)];
+    cam(:, 5) = -tilt;
+    cam(5:8, 5) = tilt;
 
-% focal length
-fprintf(fid,'%8.3f\n',f_pix);
+    % point array for testing
+    extend = -flying_height / (0.2 * f);
+    xv = linspace(X2(1) - extend, X1(4) + extend, 3);
+    yv = linspace(Y1(1) - extend, Y1(2) + extend, 3);
+    zv = linspace(0, 20, 3);
+    [X, Y, Z] = meshgrid(xv, yv, zv);
+    pts = [X(:), Y(:), Z(:)];
+    names = 101:101 + size(pts, 1) - 1;
+    pts = [names', pts];
 
-% 'initial approximations' of EOPs of photos
-str = 'A Image%i %8.3f %8.3f %8.3f %8.3f %8.3f %8.3f\n';
-for ii = 1:length(images)
-    fprintf(fid,str,ii,eop(ii,4:6),eop(ii,1:3));
-end
-fprintf(fid,'#\n');
+    % cell array to store image matrices
+    images = cell(size(cam, 1), 1);
 
-% control points and 'initial approximations' of obj space coords
-control = [1 3 5 7 9];  % which pts are control points
-str1 = '3 %i %8.3f %8.3f %8.3f   0.005   0.005   0.005\n';
-str2 = 'A %i %8.3f %8.3f %8.3f\n';
-for jj = 1:length(pts)
-    if any(jj == control)
-        fprintf(fid,str1,pts(jj,:));
-    else
-        fprintf(fid,str2,pts(jj,:));
+    % call camera stations, send to backproject, store results in cell array
+    for ii = 1:size(cam, 1)
+        images{ii} = backproject(pts, cam(ii, :), 0);
     end
-end
-fprintf(fid,'#\n0.5 0.5\n');
 
-% image points
-str = 'D %i %8.3f %8.3f\n';
-for kk = 1:length(images)
-    image = images{kk};
-    fprintf(fid,'Image%i\n',kk);  % image name
-    fprintf(fid,str,image);
+    % write results to bun2013.exe dat file
+    outfile = [
+        'bun_e' num2str(endlap * 100), ...
+        '_s' num2str(sidelap * 100), ...
+        '_h' num2str(flying_height), ...
+        '_f' num2str(f), ...
+        '_t' num2str(tilt), ...
+        '.dat'
+    ];
+    fid = fopen(outfile, 'wt');
+
+    % focal length
+    fprintf(fid, '%8.3f\n', f_pix);
+
+    % 'initial approximations' of camera stations
+    str = 'A Image%i %8.3f %8.3f %8.3f %8.3f %8.3f %8.3f\n';
+    for ii = 1:length(images)
+        fprintf(fid, str, ii, cam(ii, 4:6), cam(ii, 1:3));
+    end
     fprintf(fid,'#\n');
-end
-fprintf(fid,'#\n');
-fclose(fid);
 
-%% visualize
-% individual images
-% for pp = 1:length(images)
-%     figure(pp)
-%     hold on;
-%     grid on; axis equal;
-%     xlabel 'x [pix]';ylabel 'y [pix]';
-%     xlim([-format_x/pix/2 format_x/pix/2])
-%     ylim([-format_y/pix/2 format_y/pix/2])
-%     X = images{pp};
-%     fprintf('Plotting image %i: ',pp)
-%     fprintf('%i %i\n',size(X))
-%     plot(X(2,:),X(3,:),'k.');
-% end
-% hold off;
-% initialize object space figure
-fig = figure;
-hold on;
-title(['flying height = ' num2str(flying_height) ...
-    'm, tilt = ' num2str(tilt) '°'])
-grid on; zlim([0 flying_height+20]);
-ax = gca; ax.GridColor = [0 0 0]; ax.Projection = 'perspective';
-daspect([1 1 1])
-xlabel 'X [m]';ylabel 'Y [m]';zlabel 'Z [m]';
-plot3(pts(:,2),pts(:,3),pts(:,4),'.') % ground points
-% intersections of optical axes w/ ground
-[Xg,Yg] = meshgrid(X1,Y1); grnd = [Xg(:) Yg(:)];
-[Xg,Yg] = meshgrid(X2,Y2); grnd = [grnd;Xg(:) Yg(:)];
-plot(grnd(:,1),grnd(:,2),'k.','MarkerSize',2)
-% exposure stations and footprints
-plane_normal = [0 0 1]'; plane_normal = repmat(plane_normal,1,5);
-plane_point = [0 0 0]'; plane_point = repmat(plane_point,1,5);
-x_ = 0.5*format_x; y_ = 0.5*format_y;
-v2 = [-x_ +y_ -f]'; v1 = [+x_ +y_ -f]';
-v3 = [-x_ -y_ -f]'; v4 = [+x_ -y_ -f]';
-vx = [+x_ 0 0]'; vy = [0 +y_ 0]'; vz = [0 0 +f]';
-for nn = 1:length(eop)
-    scale = 500;
-    R = makehgtform('yrotate',deg2rad(eop(nn,5))); T = eop(nn,1:3);
-    % axes and format
-    ax = R(1:3,1:3)*[vx vy vz]; ax = scale*0.001*ax;
-    plot3([T(1) ax(1,1)+T(1)],[T(2) ax(2,1)+T(2)],...
-        [T(3) ax(3,1)+T(3)],'r','LineWidth',2)
-    plot3([T(1) ax(1,2)+T(1)],[T(2) ax(2,2)+T(2)],...
-        [T(3) ax(3,2)+T(3)],'g','LineWidth',2)
-    plot3([T(1) ax(1,3)+T(1)],[T(2) ax(2,3)+T(2)],...
-        [T(3) ax(3,3)+T(3)],'b','LineWidth',2)
-    fo = R(1:3,1:3)*([v1 v2 v3 v4 v1]+[0;0;f]); fo = scale*0.001*fo;
-    plot3(fo(1,:)+T(1),fo(2,:)+T(2),fo(3,:)+T(3),'k-','LineWidth',2)
-    % footprints
-    v = R(1:3,1:3)*[v1 v2 v3 v4 v1];
-    % conditional for high obliques: check z-orientations
-    if v(3,1) < 0 && v(3,2) < 0 && v(3,3) < 0 && v(3,4) < 0
-        % do nothing
-    elseif v(3,1) > 0 && v(3,2) < 0 && v(3,3) < 0 && v(3,4) > 0
-        v = R(1:3,1:3)*[vy+[0 0 -f]' v2 v3 -vy+[0 0 -f]'];
-    else
-        v = R(1:3,1:3)*[vy+[0 0 -f]' v1 v4 -vy+[0 0 -f]'];
+    % control points and 'initial approximations' of obj space coords
+    control = [1, 3, 5, 7, 9];  % which pts are control points
+    str1 = '3 %i %8.3f %8.3f %8.3f   0.005   0.005   0.005\n';
+    str2 = 'A %i %8.3f %8.3f %8.3f\n';
+    for jj = 1:length(pts)
+        if any(jj == control)
+            fprintf(fid, str1, pts(jj, :));
+        else
+            fprintf(fid, str2, pts(jj, :));
+        end
     end
-    for mm = 1:length(v)
-        d = dot((plane_point(:,mm)-T'),plane_normal(:,mm))/...
-            (dot(v(:,mm),plane_normal(:,mm)));
-        points(:,mm) = d*v(:,mm) + T';
-    end
-    plot(points(1,:),points(2,:),'k:','LineWidth',2)
-end
-% UI control for scaling 
-hold off;
-view([-30 30])
-% figname = ['bun_e' num2str(endlap*100) ...
-%     '_s' num2str(sidelap*100) ...
-%     '_h' num2str(flying_height) ...
-%     '_f' num2str(f) ...
-%     '_t' num2str(tilt) '.png'];
-% saveas(fig,figname)
+    fprintf(fid, '#\n0.5 0.5\n');
 
-% assignin('base','pts',pts)
-% assignin('base','eop',eop)
-% assignin('base','B',B)
-% assignin('base','W',W)
-% assignin('base','images',images)
+    % image points
+    str = 'D %i %8.3f %8.3f\n';
+    for kk = 1:length(images)
+        image = images{kk};
+        fprintf(fid, 'Image%i\n', kk);  % image name
+        fprintf(fid, str, image);
+        fprintf(fid, '#\n');
+    end
+    fprintf(fid, '#\n');
+    fclose(fid);
+
+    % % plotting stuff
+    % % individual images
+    % for pp = 1:length(images)
+    %     figure(pp)
+    %     hold on;
+    %     grid on;
+    %     axis equal;
+
+    %     xlabel 'x [pix]';
+    %     ylabel 'y [pix]';
+
+    %     xlim([-format_x / pix / 2, format_x / pix / 2])
+    %     ylim([-format_y / pix / 2, format_y / pix / 2])
+
+    %     X = images{pp};
+
+    %     fprintf('Plotting image %i: ', pp)
+    %     fprintf('%i %i\n', size(X))
+    %     plot(X(2, :), X(3, :), 'k.');
+    % end
+
+    hold off;
+
+    % initialize object space figure
+    fig = figure;
+    hold on;
+
+    title([
+        'flying height = ' num2str(flying_height), ...
+        'm, tilt = ' num2str(tilt) 'ï¿½'
+    ])
+
+    grid on;
+
+    zlim([0, flying_height + 20]);
+
+    ax = gca;
+    ax.GridColor = [0, 0, 0];
+    ax.Projection = 'perspective';
+
+    daspect([1 1 1])
+
+    xlabel 'X [m]';
+    ylabel 'Y [m]';
+    zlabel 'Z [m]';
+
+    plot3(pts(:, 2), pts(:, 3), pts(:, 4), '.')  % point array in object space
+
+    % intersections of optical axes w/ ground
+    [Xg,Yg] = meshgrid(X1, Y1);
+    grnd = [Xg(:) Yg(:)];
+    [Xg, Yg] = meshgrid(X2, Y2);
+    grnd = [grnd; Xg(:) Yg(:)];
+    plot(grnd(:, 1), grnd(:, 2), 'k.', 'MarkerSize', 2)
+
+    % camera stations and footprints
+    plane_normal = [0 0 1]';
+    plane_normal = repmat(plane_normal, 1, 5);
+    plane_point = [0 0 0]';
+    plane_point = repmat(plane_point, 1, 5);
+
+    x_ = 0.5 * format_x;
+    y_ = 0.5 * format_y;
+
+    v1 = [+x_ +y_ -f]';
+    v2 = [-x_ +y_ -f]'; 
+    v3 = [-x_ -y_ -f]';
+    v4 = [+x_ -y_ -f]';
+    vx = [+x_ 0 0]';
+    vy = [0 +y_ 0]';
+    vz = [0 0 +f]';
+
+    for nn = 1:length(cam)
+        scale = 500;
+        R = makehgtform('yrotate', deg2rad(cam(nn, 5)));
+        T = cam(nn, 1:3);
+        % axes and format
+        ax = R(1:3, 1:3)*[vx vy vz];
+        ax = scale * 0.001 * ax;
+        plot3(
+            [T(1), ax(1, 1) + T(1)], ...
+            [T(2), ax(2, 1) + T(2)], ...
+            [T(3), ax(3, 1) + T(3)], ...
+            'r', 'LineWidth', 2
+        )
+        plot3(
+            [T(1), ax(1, 2) + T(1)], ...
+            [T(2), ax(2, 2) + T(2)], ...
+            [T(3), ax(3, 2) + T(3)], ...
+            'g', 'LineWidth', 2
+        )
+        plot3(
+            [T(1), ax(1, 3) + T(1)], ...
+            [T(2), ax(2, 3) + T(2)], ...
+            [T(3), ax(3, 3) + T(3)], ...
+            'b', 'LineWidth', 2
+        )
+        fo = R(1:3, 1:3) * ([v1, v2, v3, v4, v1] + [0; 0; f]);
+        fo = scale * 0.001 * fo;  % scale from mm to m
+
+        plot3(
+            fo(1, :) + T(1), ...
+            fo(2, :) + T(2), ...
+            fo(3, :) + T(3), ...
+            'k-', 'LineWidth', 2
+        )
+
+        % footprints
+        v = R(1:3, 1:3) * [v1, v2, v3, v4, v1];
+
+        % conditional for high obliques: check z-orientations
+        if v(3, 1) < 0 && v(3, 2) < 0 && v(3, 3) < 0 && v(3, 4) < 0
+            % do nothing
+        elseif v(3, 1) > 0 && v(3, 2) < 0 && v(3, 3) < 0 && v(3, 4) > 0
+            v = R(1:3, 1:3) * [vy + [0 0 -f]' v2 v3 -vy + [0 0 -f]'];
+        else
+            v = R(1:3, 1:3) * [vy + [0 0 -f]' v1 v4 -vy+[0 0 -f]'];
+        end
+
+        for mm = 1:length(v)
+            d = dot((plane_point(:, mm) - T'), plane_normal(:, mm)) /...
+                (dot(v(:, mm), plane_normal(:, mm)));
+            points(:, mm) = d * v(:, mm) + T';
+        end
+
+        plot(points(1, :), points(2, :), 'k:', 'LineWidth', 2)
+    end
+
+    % UI control for scaling 
+    hold off;
+    view([-30 30])
+
+    % figname = [
+    %     'bun_e' num2str(endlap*100), ...
+    %     '_s' num2str(sidelap*100), ...
+    %     '_h' num2str(flying_height), ...
+    %     '_f' num2str(f), ...
+    %     '_t' num2str(tilt), ...
+    %     '.png'
+    % ];
+    %
+    % saveas(fig, figname)
+
+    % assignin('base', 'pts', pts)
+    % assignin('base', 'cam', cam)
+    % assignin('base', 'B', B)
+    % assignin('base', 'W', W)
+    % assignin('base', 'images', images)
 
 end
